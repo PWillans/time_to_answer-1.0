847def29c9146fc034aa46fa017e270870481fe9
PWillans <82551205+PWillans@users.noreply.github.com>
2021-07-29 20:03:00 -0300

    Melhorando a cara da edição.
    Pequenas alterações nos controllers.

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -4,6 +4,21 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   end
 
   def edit
-    #sfdfdfd
+    @admin = Admin.find(params[:id])
+  end
+
+  def update
+    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank? 
+      params[:admin].extract!(:password, :password_confirmation)
+    end
+
+    @admin = Admin.find(params[:id])
+    params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
+
+    if @admin.update(params_admin)
+      redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso!"
+    else
+      render :edit
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  namespace :admins_backoffice do
-    resources :admins, only: [:index]
-  end
   namespace :site do
     get 'welcome/index'
   end
@@ -10,6 +7,7 @@ Rails.application.routes.draw do
   end
   namespace :admins_backoffice do
     get 'welcome/index'
+    resources :admins, only: [:index, :edit, :update]
   end
   
   devise_for :admins
