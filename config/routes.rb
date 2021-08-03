be2731abd9c480f4729eccb23a2529cbaa4c472d
PWillans <82551205+PWillans@users.noreply.github.com>
2021-08-02 21:32:00 -0300

    Liberando o Destroy na def
    Criamos finalmente a rota para o delete.
    
    Modificamos o admins_controller para adicionar o método  destroy.
    Mudamos a rota para habilitar todas as rotas do admin.
    Demos a função ao botão de deletar.

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
     get 'welcome/index'
   end
   namespace :admins_backoffice do
-    get 'welcome/index'
-    resources :admins, except: [:delete]
+    get 'welcome/index' #Dashboard
+    resources :admins #Administradores
   end
   
   devise_for :admins
