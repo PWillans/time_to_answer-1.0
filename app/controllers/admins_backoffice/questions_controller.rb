e605aa0ae6c22402950ab6a4f43340d2df933492
PWillans <82551205+PWillans@users.noreply.github.com>
2021-08-07 19:25:36 -0300

    Criando o bom costume MVC
    Para não ferir o MVC, criamos um before_action pegando tudo o que chega no Subject.all. Criamos o método get_subjects e com isso fizemos com que ele passe pelos métodos :new e :edit  :+1:

--- app/controllers/admins_backoffice/questions_controller.rb
+++ app/controllers/admins_backoffice/questions_controller.rb
@@ -1,5 +1,6 @@
 class AdminsBackoffice::QuestionsController < AdminsBackofficeController
   before_action :set_question, only: [:edit, :update, :destroy]
+  before_action :get_subjects, only: [:new, :edit]
 
   def index
     @questions = Question.all.order(:description).page(params[:page])
@@ -12,7 +13,7 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
   def create
   @question = Question.new(params_question)
     if @question.save
-      redirect_to admins_backoffice_question_path, notice: "Questão cadastrada com sucesso!"
+      redirect_to admins_backoffice_questions_path, notice: "Questão cadastrada com sucesso!"
     else
       render :new
     end
@@ -45,5 +46,9 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
 
   def set_question
     @question = Question.find(params[:id])
+  end
+
+  def get_subjects
+    @subjects = Subject.all
   end
 end
