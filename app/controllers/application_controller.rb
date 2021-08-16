0a35e5d699e4a2d10d71da141c20fe551c4d68c2
PWillans <pwillans@gmail.com>
2021-08-16 00:36:18 -0300

    Usuário, vamos dar uma atenção
    Voltamos a interagir com a parte visual do template do Users. Mudamos o template e a parte do login do usuário.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def layout_by_resource # Metaprogramação!
+  def layout_by_resource # Metaprogramação! Condicional ternário. 
     devise_controller? ? "#{resource_class.to_s.downcase}_devise" : "application"
   end  
 end
