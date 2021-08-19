6ce4d29d4652d098db0216af52188a79877a5d28
PWillans <pwillans@gmail.com>
2021-08-19 19:08:23 -0300

    Paginação Index
    Colocamos a paginação na página Index.

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -2,5 +2,6 @@ class Site::SearchController < SiteController
   def questions
     @questions =  Question.includes(:answers)
                           .where("lower(description) LIKE ?", "%#{params[:term].downcase}%")
+                          .page(params[:page])
   end
 end
