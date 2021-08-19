33f4671c64bf31f5b2531404a9e92799ce8f1d06
PWillans <pwillans@gmail.com>
2021-08-19 19:35:41 -0300

    Refatoramos o Search
    Refatoramos a busca e usamos métodos de classe no model!

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,4 +5,14 @@ class Question < ApplicationRecord
 
   #Kaminari
   paginates_per 5
+
+  def self.search(page, term)
+    Question.includes(:answers)
+            .where("lower(description) LIKE ?", "%#{term.downcase}%")
+            .page(page)
+  end
+
+  def self.last_questions(page)
+    Question.includes(:answers).order('created_at desc').page(page)
+  end
 end
