0380dcec5edb4da2304e9d15c6fd0495399c2f94
PWillans <pwillans@gmail.com>
2021-08-24 21:15:50 -0300

    Counter_cache
    usamos o Counter_cache ferramenta poderosa do nosso RAILS. tivemos que fazer algumas mudanças pontuais para que isso fosse possível. Tais como..
    
    Model\question adc o counter_cache:true
    
    No nosso formulário, fizemos uma pequena alteração de css inline.
    
    Criamos uma nova migrate para que com isso fosse adicionado o questions_count ao subjects
    
    E no schema alterou sozinho adicionando a nova migration e colocando o questions_count

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_07_171900) do
+ActiveRecord::Schema.define(version: 2021_08_24_234133) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2021_08_07_171900) do
     t.string "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "questions_count"
   end
 
   create_table "users", force: :cascade do |t|
