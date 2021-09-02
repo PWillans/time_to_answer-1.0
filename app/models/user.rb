b9b49257374503ef9443135f150ecf19a07e924d
PWillans <pwillans@gmail.com>
2021-09-02 20:24:30 -0300

    Validações Condicionais
    Épico, usamos  o params no nosso model e isso era praticamente impossível, pq params só é acessado no nosso Controller... Pois é, mas existe uma forma de mudar isso.. Para acessar params em qualquer ambiente, lugar, cep na nossa aplicação podemos criar um método no nosso querido application_controller.. Sim la no application_controller como tudo passa por la, nada mais justo de criamos um método lá para termos acesso a toda aplicação... Criando uma before_actions  : "nome do método" podemos criar o método que quisermos e usa-la em qualquer lugar da aplicação. Ah e devemos usar o $ na frente do nosso método, para que seja um método global.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,10 @@ class User < ApplicationRecord
   # Callback
   after_create :set_statistic
 
-  #Validations
-  validates :first_name, presence: true, length: { minimum: 3 }, on: :update
-  
-  #Virtual Attributes
+  # Validations
+  validates :first_name, presence: true, length: { minimum: 3 }, on: :update, unless: :reset_password_token_present?
+
+  # Virtual Attributes
   def full_name
     "#{self.first_name} #{self.last_name}"
   end
@@ -22,5 +22,9 @@ class User < ApplicationRecord
 
   def set_statistic
     AdminStatistic.set_event(AdminStatistic::EVENTS[:total_users])
+  end
+
+  def reset_password_token_present?
+    !!$global_params[:user][:reset_password_token]
   end
 end
