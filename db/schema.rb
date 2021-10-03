3ba8a65e2c23ea8b643d1768a5a50024d9f8af08
PWillans <82551205+PWillans@users.noreply.github.com>
2021-10-03 10:41:36 -0300

    CEP
    Foi adicionado uma nova viewer, no users_backoffice edit, o campo cep agora está disponível para utilização do usuário.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_31_230326) do
+ActiveRecord::Schema.define(version: 2021_10_03_125515) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2021_08_31_230326) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "zip_code"
     t.index ["user_id"], name: "index_user_profiles_on_user_id"
   end
 
